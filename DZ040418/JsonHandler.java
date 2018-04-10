906f2f96c05479c1db70bd57171b816960222dd0
Daniil <daniilpopovw@gmail.com>
2018-04-10 23:13:36 +0300

    ДЗ

--- DZ040418/JsonHandler.java
+++ DZ040418/JsonHandler.java
@@ -2,12 +2,12 @@ package DZ040418;
 
 
 public class JsonHandler implements mainInterface {
-  
- @Override
-  public void read(String path){
-  }
-  @Override
-  public void write(String path){
-  }
-}
 
+    @Override
+    public void read(String path) {
+    }
+
+    @Override
+    public void write(String path) {
+    }
+}
--- DZ040418/XmlHandler.java
+++ DZ040418/XmlHandler.java
@@ -2,13 +2,13 @@ package DZ040418;
 
 
 public class XmlHandler implements mainInterface {
- 
-  @Override
-  public void read(String path){
-  }
-  @Override
-  public void write(String path){
-  }
- 
+
+    @Override
+    public void read(String path) {
+    }
+
+    @Override
+    public void write(String path) {
+    }
+
 }
- 
