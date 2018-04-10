f1c429b92855c5b1f60453992a7f5c6c27733d6c
DaniilPopov <37553667+DaniilPopov@users.noreply.github.com>
2018-04-10 23:15:38 +0300

    DZ
    DZ

--- DZ040418/MainHandler.java
+++ DZ040418/MainHandler.java
@@ -1,27 +1,47 @@
 package DZ040418;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class MainHandler implements mainInterface {
-   public abstract static class Handler{
-   protected String file;
-   protected Handler(String file){
-       this.file = file;
-   }
-   public static Handler getInstace(String file) {
-    if (file.endsWith(".json")) return JsonHandler(String file);
-    else retutn XmlHandler(String file);
-   }
-       
-   public static void main(String[] args) {
-    
-    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); 
-    String file = reader.readLine();
-    
-    MainHandler mh;
-    mh = MainHandler.Hendler(String file);
-       
-    mh.read(String file);
-    mh.write(String file);
-   }
+    @Override
+    public void read(String path) {
+
+    }
+
+    @Override
+    public void write(String path) {
+
+    }
+
+    public abstract static class Handler {
+        protected String file;
+
+        protected Handler(String file) {
+            this.file = file;
+        }
+
+        public static Handler getInstace(String file) {
+            if (file.endsWith(".json")) {
+                return JsonHandler(String file);
+            } else {
+                return XmlHandler(String file);
+            }
+
+        }
+
+        public static void main(String[] args) throws IOException {
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String file = reader.readLine();
+
+            MainHandler mh;
+            mh = MainHandler.Handler(String file);
+
+            mh.read(String file);
+            mh.write(String file);
+
+        }
+    }
 }
